acd81dfedfffe493b633e3947e9c76fe3d3deaef
Samuel Rey <samuel.rey.escudero@gmail.com>
2020-02-17 11:06:41 +0100

    updating save and plot functions

--- graph_enc_dec/utils.py
+++ graph_enc_dec/utils.py
@@ -37,6 +37,28 @@ def save_results(file_pref, path, data, verbose=True):
         print('SAVED as:', os.getcwd(), path)
 
 
+def plot_results(err, x_axis, legend=None, fmts=None, x_label=None):
+    median_err = np.median(err, axis=1)
+    plt.subplots()
+    for i in range(median_err.shape[1]):
+        if fmts is None:
+            plt.semilogy(x_axis, median_err[:, i])
+        else:
+            plt.semilogy(x_axis, median_err[:, i], fmts[i])
+
+    if x_label is None:
+        x_label = 'Normalized Noise Power'
+    plt.xlabel(x_label, fontsize=16)
+    plt.ylabel('Median Error', fontsize=16)
+    plt.gca().autoscale(enable=True, axis='x', tight=True)
+    plt.grid(True, which='both')
+    if legend is not None:
+        plt.legend(legend)
+        # plt.legend(legend, prop={'size': 14})
+    plt.show()
+
+
+# OLD!
 # NOTE: maybe create a class for print/plot?
 def print_partial_results(p_n, exps, node_err, med_err):
     print('Noise:', p_n)
@@ -48,151 +70,151 @@ def print_partial_results(p_n, exps, node_err, med_err):
 
 def print_results(P_n, exps, node_err, med_err):
     for i, p_n in enumerate(P_n):
-        print_partial_results(p_n, exps, node_err[:, :, i], med_err[:, :, i])
+        print_partial_results(p_n, exps, node_err[i, :, :], med_err[i, :, :])
         print()
 
 
-def save(path, exps, node_err, med_err, Gs, signals, learning):
-    if not os.path.isdir(path):
-        os.makedirs(path)
+# def save(path, exps, node_err, med_err, Gs, signals, learning):
+#     if not os.path.isdir(path):
+#         os.makedirs(path)
 
-    data = {}
-    data['Gs'] = Gs
-    data['signals'] = signals
-    data['exps'] = exps
-    data['node_err'] = node_err
-    data['median_err'] = med_err
-    data['learning'] = learning
-    timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M")
-    file_name = path.split('/')[-1] + '_{}'.format(timestamp)
-    file_path = path + '/' + file_name
-    np.save(file_path, data)
-    print('Saved as:', file_path)
-
-
-def plot_results(x, y, fmt=[], xlabel='', legend=[]):
-    # Semilogy median error
-    plt.figure(figsize=(7.5, 6))
-    for i in range(y.shape[0]):
-        plt.semilogy(x, y[i, :], fmt[i])
-    plt.xlabel(xlabel, fontsize=16)
-    plt.ylabel('Median Error', fontsize=16)
-    plt.gca().autoscale(enable=True, axis='x', tight=True)
-    plt.gca().tick_params(labelsize=16)
-    plt.grid(True)
-    plt.legend(legend, prop={'size': 14})
-    plt.show()
+#     data = {}
+#     data['Gs'] = Gs
+#     data['signals'] = signals
+#     data['exps'] = exps
+#     data['node_err'] = node_err
+#     data['median_err'] = med_err
+#     data['learning'] = learning
+#     timestamp = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M")
+#     file_name = path.split('/')[-1] + '_{}'.format(timestamp)
+#     file_path = path + '/' + file_name
+#     np.save(file_path, data)
+#     print('Saved as:', file_path)
 
 
-def save_mat_file(file_name, error, n_p, legend, fmt):
-        file_name = './results/matfiles/' + file_name
-        data = {'error': error, 'leg': legend,
-                'n_p': n_p, 'fmts': fmt}
-        savemat(file_name, data)
-        print('Saved as:', file_name)
+# # def plot_results(x, y, fmt=[], xlabel='', legend=[]):
+# #     # Semilogy median error
+# #     plt.figure(figsize=(7.5, 6))
+# #     for i in range(y.shape[0]):
+# #         plt.semilogy(x, y[i, :], fmt[i])
+# #     plt.xlabel(xlabel, fontsize=16)
+# #     plt.ylabel('Median Error', fontsize=16)
+# #     plt.gca().autoscale(enable=True, axis='x', tight=True)
+# #     plt.gca().tick_params(labelsize=16)
+# #     plt.grid(True)
+# #     plt.legend(legend, prop={'size': 14})
+# #     plt.show()
 
 
-def plot_perturbation(file_name, params=True):
-    path = PATH + 'perturbation/' + file_name + '.npy'
-    data = np.load(path).item()
-    med_err = np.median(data['median_err'], axis=0)
-    pct = [10, 20, 30, 40]
-    xlabel = '% of perturbed links'
-
-    # if params:
-    #     err = med_err[:4]
-    #     fmt = ['o-', 'P-', 'X--', 'v--']
-    #     legend = ['Enc/Dec Wei(192)', 'Enc/Dec NoUps(192)', 'AutoConv (210)',
-    #               'AutoFC (193)']
-    # else:
-    #     err = med_err[4:]
-    #     fmt = ['o-', 'P-', '^-', 'X--', 'v--']
-    #     legend = ['Enc/Dec Wei(132)', 'Enc/Dec NoUps(132)',
-    #               'Enc/Dec Wei(102)', 'AutoConv (140)', 'AutoFC (128)']
-
-    err = med_err
-    # PARA EL EXPERIMENTO QUE NO ENCUENTRO EL ARCHIVO!! 
-    # fmt = ['o-', '^-', 'P-', 'X-', 'o--', '^--', 'o:', 'P--', 'X--']
-    # legend = ['Enc/Dec Wei(738)', 'Enc/Dec NoUps(738)', 'AutoConv (720)',
-    #           'AutoFC (769)', 'Enc/Dec Wei(554)', 'Enc/Dec NoUps(554)',
-    #           'Enc/Dec Wei(132)', 'AutoConv (528)', 'AutoFC (512)']
-
-    fmt = ['X-', 'o-', 'P-', 'o--', '^--', 'P--', 'o:', '^:', 'P:']
-    legend = ['AE-FC-769', 'G-E/D-Wei-440', 'AE-CV-440',
-              'G-E/D-Wei-298', 'G-E/D-NoUps-298', 'AE-CV-280',
-              'G-E/D-Wei-132', 'G-E/D-NoUps-132', 'AE-CV-140']
-
-    plot_results(pct, err, xlabel=xlabel, fmt=fmt, legend=legend)
-    save_mat_file('perturbation', err, pct, legend, fmt)
+# def save_mat_file(file_name, error, n_p, legend, fmt):
+#         file_name = './results/matfiles/' + file_name
+#         data = {'error': error, 'leg': legend,
+#                 'n_p': n_p, 'fmts': fmt}
+#         savemat(file_name, data)
+#         print('Saved as:', file_name)
 
 
-def plot_noise(file_name, node_err=True):
-    # path = PATH + 'diff_models/' + file_name + '.npy'
-    path = PATH + 'noise/' + file_name + '.npy'
-    data = np.load(path).item()
-    noise = data['signals']['noise']
-    # med_err = np.median(data['median_err'], axis=0)[1:]
-    if node_err:
-        med_err = np.median(data['node_err'], axis=0)
-        mat_file = 'noise_node_err'
-    else:
-        med_err = np.median(data['median_err'], axis=0)
-        mat_file = 'noise'
-    xlabel = 'Normalized noise power'
+# def plot_perturbation(file_name, params=True):
+#     path = PATH + 'perturbation/' + file_name + '.npy'
+#     data = np.load(path).item()
+#     med_err = np.median(data['median_err'], axis=0)
+#     pct = [10, 20, 30, 40]
+#     xlabel = '% of perturbed links'
 
-    # legend = ['Enc/Dec Wei(192)', 'AutoConv (210)',
-    #           'AutoFC (193)', 'Enc/Dec Wei(132)', 'Enc/Dec Wei(102)',
-    #           'AutoConv (140)']
-    # fmt = ['o-', 'P-', 'X-', 'o--', 'o:', 'P--']
+#     # if params:
+#     #     err = med_err[:4]
+#     #     fmt = ['o-', 'P-', 'X--', 'v--']
+#     #     legend = ['Enc/Dec Wei(192)', 'Enc/Dec NoUps(192)', 'AutoConv (210)',
+#     #               'AutoFC (193)']
+#     # else:
+#     #     err = med_err[4:]
+#     #     fmt = ['o-', 'P-', '^-', 'X--', 'v--']
+#     #     legend = ['Enc/Dec Wei(132)', 'Enc/Dec NoUps(132)',
+#     #               'Enc/Dec Wei(102)', 'AutoConv (140)', 'AutoFC (128)']
 
-    legend = ['AE-FC-709', 'G-E/D-440', 'AE-CV-429', 'G-E/D-298', 'AE-CV-308',
-              'G-E/D-132', 'AE-CV-144']
-    fmt = ['X-', 'o-', 'P-', 'o--', 'P--', 'o:', 'P:']
+#     err = med_err
+#     # PARA EL EXPERIMENTO QUE NO ENCUENTRO EL ARCHIVO!! 
+#     # fmt = ['o-', '^-', 'P-', 'X-', 'o--', '^--', 'o:', 'P--', 'X--']
+#     # legend = ['Enc/Dec Wei(738)', 'Enc/Dec NoUps(738)', 'AutoConv (720)',
+#     #           'AutoFC (769)', 'Enc/Dec Wei(554)', 'Enc/Dec NoUps(554)',
+#     #           'Enc/Dec Wei(132)', 'AutoConv (528)', 'AutoFC (512)']
 
-    plot_results(noise, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
-    save_mat_file('mat_file', med_err, noise, legend, fmt)
+#     fmt = ['X-', 'o-', 'P-', 'o--', '^--', 'P--', 'o:', '^:', 'P:']
+#     legend = ['AE-FC-769', 'G-E/D-Wei-440', 'AE-CV-440',
+#               'G-E/D-Wei-298', 'G-E/D-NoUps-298', 'AE-CV-280',
+#               'G-E/D-Wei-132', 'G-E/D-NoUps-132', 'AE-CV-140']
 
-
-def plot_node_pert(file_name, node_err=True):
-    path = PATH + 'node_pert/' + file_name + '.npy'
-    data = np.load(path).item()
-    pct = np.array([10, 20, 30, 40, 50])
-    print((256-pct))
-    if node_err:
-        med_err = np.median(data['node_err'], axis=0)
-        mat_file = 'nodes_pert_node_err'
-    else:
-        med_err = np.median(data['median_err'], axis=0)
-        mat_file = 'nodes_pert'
-    xlabel = 'Number of deleted nodes'
-    legend = ['AE-FC-709', 'G-E/D-440', 'G-E/D-298', 'AE-CV-308',
-              'G-E/D-132', 'AE-CV-144']
-    fmt = ['X-', 'o-', 'o--', 'P--', 'o:', 'P:']
-
-    # med_err = med_err[np.array([0, 2, 3, 4, 5])]
-    legend.remove(legend[1])
-    fmt.remove(fmt[1])
-
-    plot_results(pct, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
-    save_mat_file(mat_file, med_err, pct, legend, fmt)
+#     plot_results(pct, err, xlabel=xlabel, fmt=fmt, legend=legend)
+#     save_mat_file('perturbation', err, pct, legend, fmt)
 
 
-def plot_deltas(file_name, node_err=True):
-    path = PATH + 'deltas/' + file_name + '.npy'
-    data = np.load(path).item()
-    deltas = [4, 40, 80, 120, 160, 200]
-    if node_err:
-        med_err = np.median(data['node_err'], axis=0)
-        mat_file = 'deltas_node_err'
-    else:
-        med_err = np.median(data['median_err'], axis=0)
-        mat_file = 'deltas'
-    xlabel = 'Number of deltas'
-    legend = ['AE-FC-709', 'G-E/D-298', 'AE-CV-308',
-              'G-E/D-132', 'AE-CV-144']
-    fmt = ['X-', 'o--', 'P--', 'o:', 'P:']
-    plot_results(deltas, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
-    save_mat_file(mat_file, med_err, deltas, legend, fmt)
+# def plot_noise(file_name, node_err=True):
+#     # path = PATH + 'diff_models/' + file_name + '.npy'
+#     path = PATH + 'noise/' + file_name + '.npy'
+#     data = np.load(path).item()
+#     noise = data['signals']['noise']
+#     # med_err = np.median(data['median_err'], axis=0)[1:]
+#     if node_err:
+#         med_err = np.median(data['node_err'], axis=0)
+#         mat_file = 'noise_node_err'
+#     else:
+#         med_err = np.median(data['median_err'], axis=0)
+#         mat_file = 'noise'
+#     xlabel = 'Normalized noise power'
+
+#     # legend = ['Enc/Dec Wei(192)', 'AutoConv (210)',
+#     #           'AutoFC (193)', 'Enc/Dec Wei(132)', 'Enc/Dec Wei(102)',
+#     #           'AutoConv (140)']
+#     # fmt = ['o-', 'P-', 'X-', 'o--', 'o:', 'P--']
+
+#     legend = ['AE-FC-709', 'G-E/D-440', 'AE-CV-429', 'G-E/D-298', 'AE-CV-308',
+#               'G-E/D-132', 'AE-CV-144']
+#     fmt = ['X-', 'o-', 'P-', 'o--', 'P--', 'o:', 'P:']
+
+#     plot_results(noise, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
+#     save_mat_file('mat_file', med_err, noise, legend, fmt)
+
+
+# def plot_node_pert(file_name, node_err=True):
+#     path = PATH + 'node_pert/' + file_name + '.npy'
+#     data = np.load(path).item()
+#     pct = np.array([10, 20, 30, 40, 50])
+#     print((256-pct))
+#     if node_err:
+#         med_err = np.median(data['node_err'], axis=0)
+#         mat_file = 'nodes_pert_node_err'
+#     else:
+#         med_err = np.median(data['median_err'], axis=0)
+#         mat_file = 'nodes_pert'
+#     xlabel = 'Number of deleted nodes'
+#     legend = ['AE-FC-709', 'G-E/D-440', 'G-E/D-298', 'AE-CV-308',
+#               'G-E/D-132', 'AE-CV-144']
+#     fmt = ['X-', 'o-', 'o--', 'P--', 'o:', 'P:']
+
+#     # med_err = med_err[np.array([0, 2, 3, 4, 5])]
+#     legend.remove(legend[1])
+#     fmt.remove(fmt[1])
+
+#     plot_results(pct, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
+#     save_mat_file(mat_file, med_err, pct, legend, fmt)
+
+
+# def plot_deltas(file_name, node_err=True):
+#     path = PATH + 'deltas/' + file_name + '.npy'
+#     data = np.load(path).item()
+#     deltas = [4, 40, 80, 120, 160, 200]
+#     if node_err:
+#         med_err = np.median(data['node_err'], axis=0)
+#         mat_file = 'deltas_node_err'
+#     else:
+#         med_err = np.median(data['median_err'], axis=0)
+#         mat_file = 'deltas'
+#     xlabel = 'Number of deltas'
+#     legend = ['AE-FC-709', 'G-E/D-298', 'AE-CV-308',
+#               'G-E/D-132', 'AE-CV-144']
+#     fmt = ['X-', 'o--', 'P--', 'o:', 'P:']
+#     plot_results(deltas, med_err, xlabel=xlabel, fmt=fmt, legend=legend)
+#     save_mat_file(mat_file, med_err, deltas, legend, fmt)
 
 
 if __name__ == '__main__':
